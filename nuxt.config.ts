74954a99ff04eefc2635c448a9b1101be6e0d39c
William Chong <me@williamchong.cloud>
2025-12-09 16:38:28 +0800

    ⚡️ Remove sentry tracing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,6 +76,13 @@ export default defineNuxtConfig({
     }
   },
 
+  vite: {
+    define: {
+      __SENTRY_DEBUG__: false,
+      __SENTRY_TRACING__: false,
+    },
+  },
+
   sourcemap: {
     client: 'hidden'
   }
